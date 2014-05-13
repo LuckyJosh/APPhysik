79763ec1a4a09827ad38368f44384037f2edaf35
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2014-05-13 21:53:12 +0200

    Messwerte

---
+++ FranckHertz[ ]/Auswertung.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Josh
+
+"""
+
+from __future__ import (print_function,
+                        division,
+                        unicode_literals,
+                        absolute_import)
+import math as m
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+from scipy.optimize import curve_fit
+from sympy import *
+import uncertainties as unc
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+
+if not "..\_globales\python" in sys.path:
+	sys.path.append("..\_globales\python")
+
+from aputils.utils import Quantity, ErrorEquation
+from aputils.latextables.tables import Table
+
+
+## Print Funktionen
