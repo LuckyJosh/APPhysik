ab36b037b9fe4605ea71736757e65f3c29318882
Joshua Luckey <joshua.luckey@tu-dortmund.de>
2014-06-17 23:02:28 +0200

    Messwerte

---
+++ Millikan[ ]/Auswertung.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Josh
+
+"""
+
+from __future__ import (print_function,
+                        division,
+                        unicode_literals,
+                        absolute_import)
+import math as m
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+from scipy.optimize import curve_fit
+import sympy as sym
+import uncertainties as unc
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+
+from aputils.utils import Quantity, ErrorEquation
+from aputils.latextables.tables import Table
+
+
+## Print Funktionen
